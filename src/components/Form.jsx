b805a7c54cc5da9b0c9606cde2c4b8826637f52b
DannyClark90 <dannyclark_90@hotmail.co.uk>
2023-12-01 15:29:35 +0000

    To do list update.

---
+++ src/components/Form.jsx
@@ -0,0 +1,5 @@
+const FormInput = () => {
+    return <form><label htmlFor="input"><input type="text"  placeholder="Task to add"/></label></form>
+}
+
+export default FormInput
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react"
 import ToDoCard from "./ToDoCard"
+import FormInput from "./Form"
 
-const ToDoList = () => {
+const ToDoList = (event) => {
   const [tasks, setTasks] = useState([
     { task: "task1", id: 1 },
     { task: "task2", id: 2 },
@@ -13,9 +14,10 @@ const ToDoList = () => {
       <h2>This is the to-do list</h2>
       <ul>
         {tasks.map((task) => {
-          return <li key={task.id}>{<ToDoCard task={task.task} />}</li>
+          return <li key={task.id}>{<ToDoCard task={task} />}</li>
         })}
       </ul>
+      <FormInput />
     </>
   )
 }
